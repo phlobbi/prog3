e40efc245d4d7c8507f802a837f7126bc7f74f4a
dgomer <danielgomer25@gmail.com>
2023-01-02 21:05:38 +0100

    car interface with setter and getter

---
+++ src/main/java/de/htwsaar/hopper/logic/interfaces/CarInterface.java
@@ -0,0 +1,61 @@
+package de.htwsaar.hopper.logic.interfaces;
+
+public interface CarInterface {
+
+    /*
+        GETTER
+     */
+
+    //  Auto ID
+    int getCarId();
+
+    //  Marke
+    String getBrand();
+
+    //  Erstellungsdatum
+    String getCreationDate();
+
+    //  Sitze
+    int getSeats();
+
+    //  Grundpreis
+    double getBasePrice();
+
+    //  Tagespreis
+    double getCurrentPrice();
+
+    //  Kennzeichen
+    String getLicensePlate();
+
+    //  Modell
+    String getModel();
+
+
+    /*
+        SETTER
+     */
+
+    //  Auto ID
+    void setCarId(int carId);
+
+    //  Marke
+    void setBrand(String brand);
+
+    //  Erstellungsdatum
+    void setCreationDate(String creationDate);
+
+    //  Sitze
+    void setSeats(int seats);
+
+    //  Grundpreis
+    void setBasePrice(double basePrice);
+
+    //  Tagespreis
+    void setCurrentPrice(double currentPrice);
+
+    //  Kennzeichen
+    void setLicensePlate(String licensePlate);
+
+    //  Modell
+    void setModel(String model);
+}
