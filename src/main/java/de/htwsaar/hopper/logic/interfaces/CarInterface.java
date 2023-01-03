a70e6537fd8ab656848f44089944d97abc20dc8a
Bennet <117591212+FriendlyDiamond@users.noreply.github.com>
2023-01-03 10:19:05 +0100

    removed the comments

--- src/main/java/de/htwsaar/hopper/logic/interfaces/CarInterface.java
+++ src/main/java/de/htwsaar/hopper/logic/interfaces/CarInterface.java
@@ -2,68 +2,27 @@ package de.htwsaar.hopper.logic.interfaces;
 
 public interface CarInterface {
 
-    /*
-        GETTER
-     */
-
-    //  Auto ID
+    /*  GETTER  */
     int getCarId();
-
-    //  Marke
     String getBrand();
-
-    //  Erstellungsdatum
     String getCreationDate();
-
-    //  Sitze
     int getSeats();
-
-    //  Grundpreis
     double getBasePrice();
-
-    //  Tagespreis
     double getCurrentPrice();
-
-    //  Kennzeichen
     String getLicensePlate();
-
-    //  Modell
     String getModel();
 
 
-    /*
-        SETTER
-     */
-
-    //  Auto ID
+    /*  SETTER  */
     void setCarId(int carId);
-
-    //  Marke
     void setBrand(String brand);
-
-    //  Erstellungsdatum
     void setCreationDate(String creationDate);
-
-    //  Sitze
     void setSeats(int seats);
-
-    //  Grundpreis
     void setBasePrice(double basePrice);
-
-    //  Tagespreis
     void setCurrentPrice(double currentPrice);
-
-    //  Kennzeichen
     void setLicensePlate(String licensePlate);
-
-    //  Modell
     void setModel(String model);
-
-
-    /*
-        toString
-     */
-
-    //toString
+    
+    /*  toString  */
     String toString();
 }
