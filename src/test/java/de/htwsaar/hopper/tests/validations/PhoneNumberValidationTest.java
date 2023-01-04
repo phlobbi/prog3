9f0fa2c24913053d40df7d7df7c513233787e58f
Philip Dausend <dev@philip-dausend.de>
2023-01-04 18:09:49 +0100

    Added additional test cases to PhoneNumberValidationTest

--- src/test/java/de/htwsaar/hopper/tests/validations/PhoneNumberValidationTest.java
+++ src/test/java/de/htwsaar/hopper/tests/validations/PhoneNumberValidationTest.java
@@ -8,25 +8,25 @@ import static org.junit.Assert.*;
 public class PhoneNumberValidationTest {
 
     @Test
-    public void phoneNumberAccordingToPatternWithBackslashShouldBeTrue(){
+    public void phoneNumberAccordingToPatternWithSlashShouldBeTrue(){
         String phoneNumber = "0176/12345678";
         assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
     }
 
     @Test
-    public void phoneNumberAccordingToPatternAndLeadingZeroAndWithoutBackslashShouldBeTrue(){
+    public void phoneNumberAccordingToPatternAndLeadingZeroAndWithoutSlashShouldBeTrue(){
         String phoneNumber = "017612345678";
         assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
     }
 
     @Test
-    public void phoneNumberAccordingToPatternAndLeadingPlus49AndWithBackslashShouldBeTrue(){
+    public void phoneNumberAccordingToPatternAndLeadingPlus49AndWithSlashShouldBeTrue(){
         String phoneNumber = "+49176/12345678";
         assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
     }
 
     @Test
-    public void phoneNumberAccordingToPatternAndWithPlus49AndWithoutBackslashShouldBeTrue(){
+    public void phoneNumberAccordingToPatternAndWithPlus49AndWithoutSlashShouldBeTrue(){
         String phoneNumber = "+4917612345678";
         assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
     }
@@ -71,5 +71,40 @@ public class PhoneNumberValidationTest {
     public void phoneNumberOnlyConsistingOfLettersShouldThrowException(){
         String phoneNumber = "geilomatico";
         Validation.validatePhonenumber(phoneNumber);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void phoneNumberOnlyConsistingOfSpecialCharactersShouldThrowException(){
+        String phoneNumber = "++-";
+        Validation.validatePhonenumber(phoneNumber);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void nullPhoneNumberShouldThrowException(){
+        Validation.validatePhonenumber(null);
+    }
+
+    @Test
+    public void phoneNumberWithFourDigitRegionShouldBeTrue(){
+        String phoneNumber = "01763/12345678";
+        assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
+    }
+
+    @Test
+    public void phoneNumberWithFourDigitRegionInternationalShouldBeTrue(){
+        String phoneNumber = "+491763/12345678";
+        assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
+    }
+
+    @Test
+    public void phoneNumberWithFourDigitRegionInternationalWithoutSlashShouldBeTrue(){
+        String phoneNumber = "+49176312345678";
+        assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
+    }
+
+    @Test
+    public void phoneNumberWithFourDigitRegionWithoutSlashShouldBeTrue(){
+        String phoneNumber = "0176312345678";
+        assertEquals(Validation.validatePhonenumber(phoneNumber), phoneNumber.trim());
     }
 }
