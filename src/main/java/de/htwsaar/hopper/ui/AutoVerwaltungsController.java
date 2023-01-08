666da74d3a9328418c7f8446e3677a3a63a34727
Anderson <105352771+Nguegan@users.noreply.github.com>
2023-01-08 08:05:37 +0100

    Update src/main/java/de/htwsaar/hopper/ui/AutoVerwaltungsController.java
    Co-authored-by: Philip Dausend <dev@philip-dausend.de>

--- src/main/java/de/htwsaar/hopper/ui/AutoVerwaltungsController.java
+++ src/main/java/de/htwsaar/hopper/ui/AutoVerwaltungsController.java
@@ -9,7 +9,7 @@ import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 
-public class AutoVerwaltungsController {
+public class CarManagementController {
 
     @FXML
     private Button bntRead;
