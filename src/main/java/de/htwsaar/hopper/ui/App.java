4dab5290768241f1af70a96b3995b56f805b5753
Philip Dausend <dev@philip-dausend.de>
2023-01-16 13:48:52 +0100

    Fix potential problems

--- src/main/java/de/htwsaar/hopper/ui/App.java
+++ src/main/java/de/htwsaar/hopper/ui/App.java
@@ -35,9 +35,13 @@ public final class App extends Application {
 	 */
 	public static void setRoot(String fxmlfile)   {
 		try {
-			URL url = App.class.getResource("/"+fxmlfile+"")  ;
-			root = FXMLLoader.load(url) ;
-			scene.setRoot(root);
+			URL url = App.class.getResource("/"+fxmlfile);
+			if (url != null) {
+				root = FXMLLoader.load(url);
+				scene.setRoot(root);
+			} else {
+				throw new IOException("Could not load FXML file.");
+			}
 		} catch (IOException exception){
 			System.out.println("Die fxml-Datei konnte nicht geladen werden");
 		}
