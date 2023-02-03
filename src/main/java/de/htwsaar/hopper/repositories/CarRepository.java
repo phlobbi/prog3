3a282b5141f4c5e9c255dcb7fac1973ab03e17af
gitroba <roba0605@web.de>
2023-02-03 17:58:09 +0100

    fix unnecessary imports

--- src/main/java/de/htwsaar/hopper/repositories/CarRepository.java
+++ src/main/java/de/htwsaar/hopper/repositories/CarRepository.java
@@ -1,6 +1,5 @@
 package de.htwsaar.hopper.repositories;
 
-import de.htwsaar.hopper.logic.enums.CarTypeEnum;
 import de.htwsaar.hopper.logic.implementations.Booking;
 import de.htwsaar.hopper.logic.implementations.Car;
 
@@ -8,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import java.util.Calendar;
 import java.util.List;
 
 /**
