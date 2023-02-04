9ee94af0153cdae898d856a028a3117d83a1f1a1
Nguegan <darelatsafack123456@gmail.com>
2023-02-04 12:35:59 +0100

    controller für carshow.fxml .

---
+++ src/main/java/de/htwsaar/hopper/ui/CarShowController.java
@@ -0,0 +1,74 @@
+package de.htwsaar.hopper.ui;
+
+import de.htwsaar.hopper.logic.implementations.Car;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+import de.htwsaar.hopper.repositories.CarRepository ;
+
+
+public class CarShowController  implements Initializable {
+
+    @FXML
+    private Button btnGoBack;
+
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private Label labelCarBasePrise ;
+
+    @FXML
+    private Label labelCarModel;
+    @FXML
+    private Label labelCarCreationDate;
+
+    @FXML
+    private Label labelCarCurrentPrise;
+
+    @FXML
+    private Label labelCarSeats;
+
+    @FXML
+    private Label labelCarType;
+
+    @FXML
+    void removeCar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToCarView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateCar(ActionEvent event) {
+
+    }
+
+
+    /**
+     * @param url
+     * @param resourceBundle
+     * Diese Methode initialisiert die Informationen über das ausgewählte Auto
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        int id = CarManagementController.getInput() ;
+        Car car = CarRepository.find(id) ;
+        labelCarBasePrise.setText(String.valueOf(car.getBasePrice()));
+        labelCarModel.setText(String.valueOf(car.getModel()));
+        labelCarCreationDate.setText(String.valueOf(car.getCreationDate()));
+        labelCarCurrentPrise.setText(String.valueOf(car.getCurrentPrice()));
+        labelCarSeats.setText(String.valueOf(car.getSeats()));
+        labelCarType.setText(String.valueOf(car.getType()));
+    }
+}
