b3b68cf8e6626f0236f6508b999f19d2082caa66
Nguegan <darelatsafack123456@gmail.com>
2023-02-07 14:23:57 +0100

    Car-view.fxml responsiv

--- src/main/java/de/htwsaar/hopper/ui/CarManagementController.java
+++ src/main/java/de/htwsaar/hopper/ui/CarManagementController.java
@@ -2,14 +2,21 @@ package de.htwsaar.hopper.ui;
 
 
 import de.htwsaar.hopper.logic.implementations.Car;
+import de.htwsaar.hopper.repositories.CarRepository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public final class CarManagementController {
+public final class CarManagementController implements Initializable {
     private static Car selectedCar ;
 
     @FXML
@@ -28,7 +35,7 @@ public final class CarManagementController {
     private Button btnUpdate;
 
     @FXML
-    private TableView<?> tableView;
+    private TableView<Car> tableView;
 
     @FXML
     void switchToSceneAddCar(ActionEvent event) {
@@ -37,8 +44,10 @@ public final class CarManagementController {
     }
 
     @FXML
-    void switchToSceneReadCar(ActionEvent event) {
-
+    void switchToSceneReadCar(ActionEvent event) throws IOException {
+        Car car = tableView.getSelectionModel().getSelectedItem();
+        selectedCar = car ;
+        App.setRoot("carshow.fxml");
     }
 
     @FXML
@@ -65,6 +74,23 @@ public final class CarManagementController {
 
     }
 
+    /**
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        carBrandColumn.setCellValueFactory(new PropertyValueFactory<>("brand"));
+        carTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        carIdColumn.setCellValueFactory(new PropertyValueFactory<>("carId"));
+        ObservableList<Car> observableList = FXCollections.observableArrayList() ;
+        observableList.addAll(CarRepository.findAll()) ;
+        tableView.getItems().addAll(observableList) ;
+        tableView.getSelectionModel().select(0);
+
+    }
 }
 
 
