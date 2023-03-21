ff8f3a3895674e8f07fd6c1583020361e8e4a9db
Philip Dausend <dev@philip-dausend.de>
2023-03-21 18:13:19 +0100

    Implement main billing functionality
    ## Implementations
    - Implemented writeBillingLine(), which writes a billing description and its amount in the correct line. It also adds the amount to the "total" variable.
    - Implemented calculateLinePosition(), which calculates the y-axis position of the next free line in the invoice
    - Implemented calculateDaysBetween(), which calculates the days between two Calendar objects (including end date)
    - Implemented clearHourMinuteSecond(), which clears unnecessary time data from Calendar objects
    - Implemented isSameDate(), which checks if two Calendar objects have the same date
    ## Changes
    - writeInvoiceInformation() now writes the actual customer data
    - Replaced every occurrence of "contentstream" with "contentStream" to ensure consistency

--- src/main/java/de/htwsaar/hopper/logic/implementations/Invoice.java
+++ src/main/java/de/htwsaar/hopper/logic/implementations/Invoice.java
@@ -1,6 +1,9 @@
 package de.htwsaar.hopper.logic.implementations;
 
+import de.htwsaar.hopper.logic.enums.CarTypeEnum;
 import de.htwsaar.hopper.logic.interfaces.BookingInterface;
+import de.htwsaar.hopper.logic.interfaces.CarInterface;
+import de.htwsaar.hopper.logic.interfaces.CustomerInterface;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -9,7 +12,9 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 
 /**
@@ -18,14 +23,37 @@ import java.util.Calendar;
  * @author philipdausend
  */
 public class Invoice {
+    private static final double STRAFZUSCHLAG = 0.5;
+    private static final double STEUERSATZ = 0.19;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
     private final BookingInterface booking;
+    private final CarInterface associatedCar;
+    private final CustomerInterface associatedCustomer;
+    private int linePosition;
+    private double total;
 
     /**
      * Konstruktor, der eine neue Rechnung erstellt.
      * @param booking Buchung, für die die Rechnung erstellt werden soll
      */
     public Invoice(BookingInterface booking) {
-        this.booking = new Booking(5, 5, Calendar.getInstance(), Calendar.getInstance());
+        Calendar expirationDate = Calendar.getInstance();
+        Calendar productionDate = Calendar.getInstance();
+        Calendar pickUpDay = Calendar.getInstance();
+        Calendar dropOffDay = Calendar.getInstance();
+        Calendar realDropOffDay = Calendar.getInstance();
+        expirationDate.add(Calendar.YEAR, 1);
+        productionDate.add(Calendar.YEAR, -2);
+        pickUpDay.set(2023, Calendar.JANUARY, 1);
+        dropOffDay.set(2023, Calendar.JANUARY, 3);
+        realDropOffDay.set(2023, Calendar.JANUARY, 5);
+        this.booking = new Booking(5, 5, pickUpDay, dropOffDay);
+        this.booking.setRealDropOffDate(realDropOffDay);
+        this.associatedCar = new Car(CarTypeEnum.AUTO, "Audi", productionDate, 5, 100, 20, "SB-AB-23", "A4");
+        this.associatedCustomer = new Customer("Max", "Mustermann", "max@muster.de", "Musterstraße", "1", "66130", "Saarbrücken", "0176/12345678", "DE06500105177825353352", "L01C0097Z31", expirationDate);
+        this.linePosition = 0;
+        this.total = 0.0;
     }
 
     /**
@@ -44,6 +72,27 @@ public class Invoice {
                 contentStream.setFont(PDType1Font.HELVETICA, 10);
                 writeStoreAddress(contentStream);
                 writeInvoiceInformation(contentStream);
+                int bookedDays = calculateDaysBetween(booking.getPickUpDate(), booking.getDropOffDate());
+                writeBillingLine(
+                        contentStream,
+                        String.format("%s %s - Grundbetrag Miete",
+                                associatedCar.getBrand(), associatedCar.getModel()),
+                        associatedCar.getBasePrice());
+                writeBillingLine(
+                        contentStream,
+                        String.format("%s %s - Tagespreis Miete (Anzahl Tage: %d)",
+                                associatedCar.getBrand(), associatedCar.getModel(),
+                                bookedDays),
+                associatedCar.getCurrentPrice() * bookedDays);
+                if(!isSameDate(booking.getDropOffDate(), booking.getRealDropOffDate())) {
+                    int lateDays = calculateDaysBetween(booking.getDropOffDate(), booking.getRealDropOffDate());
+                    writeBillingLine(
+                            contentStream,
+                            String.format("%s %s - Strafzuschlag \"Überzogene Miete\" (Anzahl Tage: %d)",
+                                    associatedCar.getBrand(), associatedCar.getModel(),
+                                    lateDays - 1),
+                            associatedCar.getCurrentPrice() * (lateDays - 1) * STRAFZUSCHLAG);
+                }
                 contentStream.close();
                 doc.save(new File("generated-invoice.pdf"));
 
@@ -57,40 +106,59 @@ public class Invoice {
 
     /**
      * Schreibt die Adresse des Autovermieters in die PDF-Rechnung.
-     * @param contentstream Contentstream der PDF-Rechnung
+     * @param contentStream Contentstream der PDF-Rechnung
      * @throws IOException Falls es beim Schreiben zu einem Fehler kommt
      */
-    private void writeStoreAddress(PDPageContentStream contentstream) throws IOException {
-        contentstream.beginText();
-        contentstream.newLineAtOffset(51, 710);
-        contentstream.showText("Hopper's Autovermietung");
-        contentstream.newLineAtOffset(0, -12);
-        contentstream.showText("Hauptstraße 1");
-        contentstream.newLineAtOffset(0, -12);
-        contentstream.showText("12345 Hopperstadt");
-        contentstream.endText();
+    private void writeStoreAddress(PDPageContentStream contentStream) throws IOException {
+        contentStream.beginText();
+        contentStream.newLineAtOffset(51, 710);
+        contentStream.showText("Hopper's Autovermietung");
+        contentStream.newLineAtOffset(0, -12);
+        contentStream.showText("Hauptstraße 1");
+        contentStream.newLineAtOffset(0, -12);
+        contentStream.showText("12345 Hopperstadt");
+        contentStream.endText();
     }
 
     /**
      * Schreibt die Rechnungsdaten in die PDF-Rechnung.
-     * @param contentstream Contentstream der PDF-Rechnung
+     * @param contentStream Contentstream der PDF-Rechnung
      * @throws IOException Falls es beim Schreiben zu einem Fehler kommt
      */
-    private void writeInvoiceInformation(PDPageContentStream contentstream) throws IOException {
-        contentstream.beginText();
-        contentstream.newLineAtOffset(340, 735);
-        contentstream.showText(formatDate(Calendar.getInstance()));
-        contentstream.newLineAtOffset(0, -12);
+    private void writeInvoiceInformation(PDPageContentStream contentStream) throws IOException {
+        contentStream.beginText();
+        contentStream.newLineAtOffset(340, 735);
+        contentStream.showText(formatDate(Calendar.getInstance()));
+        contentStream.newLineAtOffset(0, -12);
         // TODO Zeile entkommentieren, sobald das Testen abgeschlossen ist
         // contentstream.showText(String.valueOf(booking.getBookingId()));
-        contentstream.showText("123");
-        contentstream.newLineAtOffset(0, -38);
-        contentstream.showText("Max Mustermann");
-        contentstream.newLineAtOffset(0, -12);
-        contentstream.showText("Musterstraße 1");
-        contentstream.newLineAtOffset(0, -12);
-        contentstream.showText("12345 Musterstadt");
-        contentstream.endText();
+        contentStream.showText("123");
+        contentStream.newLineAtOffset(0, -38);
+        contentStream.showText(associatedCustomer.getFirstName() + " " + associatedCustomer.getLastName());
+        contentStream.newLineAtOffset(0, -12);
+        contentStream.showText(associatedCustomer.getStreet() + " " + associatedCustomer.getHouseNumber());
+        contentStream.newLineAtOffset(0, -12);
+        contentStream.showText(associatedCustomer.getZipCode() + " " + associatedCustomer.getCity());
+        contentStream.endText();
+    }
+
+    /**
+     * Schreibt eine Zeile in die PDF-Rechnung.
+     * @param contentStream Contentstream der PDF-Rechnung
+     * @param description Beschreibung der Zeile
+     * @param amount Betrag der Zeile
+     * @throws IOException Falls es beim Schreiben zu einem Fehler kommt
+     */
+    private void writeBillingLine(PDPageContentStream contentStream, String description, double amount) throws IOException {
+        double rounded = Math.round(amount * 100.0) / 100.0;
+        contentStream.beginText();
+        contentStream.newLineAtOffset(52, calculateLinePosition());
+        contentStream.showText(description);
+        contentStream.newLineAtOffset(467, 0);
+        contentStream.showText(df.format(amount));
+        contentStream.endText();
+        linePosition++;
+        total += rounded;
     }
 
     /**
@@ -101,6 +169,44 @@ public class Invoice {
     private String formatDate(Calendar date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         return dateFormat.format(date.getTime());
+    }
+
+    /**
+     * Berechnet die Position der nächsten Zeile in der PDF-Rechnung.
+     * @return Y-Position der nächsten Zeile
+     */
+    private int calculateLinePosition() {
+        return 572 - (linePosition * 26);
+    }
+
+    /**
+     * Berechnet die Anzahl der Tage zwischen zwei Kalenderdaten.
+     * Die Methode berücksichtigt dabei, dass das Enddatum inklusive ist.
+     * Beispiel: 01.01.2020 - 03.01.2020 = 3 Tage
+     * @param start Startdatum
+     * @param end Enddatum
+     * @return Anzahl der Tage zwischen den beiden Kalenderdaten
+     */
+    private int calculateDaysBetween(Calendar start, Calendar end) {
+        clearHourMinuteSecond(start);
+        clearHourMinuteSecond(end);
+        return (int) (ChronoUnit.DAYS.between(start.toInstant(), end.toInstant()) + 1);
+    }
+
+    /**
+     * Setzt die Stunden, Minuten und Sekunden eines Kalenderdatums auf 0.
+     * @param date Kalenderdatum, das bereinigt werden soll
+     */
+    private void clearHourMinuteSecond(Calendar date) {
+        date.set(Calendar.HOUR_OF_DAY, 0);
+        date.set(Calendar.MINUTE, 0);
+        date.set(Calendar.SECOND, 0);
+        date.set(Calendar.MILLISECOND, 0);
+    }
+
+    private boolean isSameDate(Calendar date1, Calendar date2) {
+        return date1.get(Calendar.YEAR) == date2.get(Calendar.YEAR) &&
+                date1.get(Calendar.DAY_OF_YEAR) == date2.get(Calendar.DAY_OF_YEAR);
     }
 
     /**
