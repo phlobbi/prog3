ae44e8fb9c09b4812b6b7d7ba107049caa92e486
dgomer <danielgomer25@gmail.com>
2023-03-22 11:32:21 +0100

    Added changeLanguage method, properties data and a testbutton

--- src/main/java/de/htwsaar/hopper/ui/FirstViewController.java
+++ src/main/java/de/htwsaar/hopper/ui/FirstViewController.java
@@ -4,8 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public final class FirstViewController {
+    boolean isEnglish = false;
 
     /**
      * Wechselt bei Aufruf auf die Buchungsverwaltung.
@@ -41,5 +44,22 @@ public final class FirstViewController {
     @FXML
     void switchToCarManagementView(MouseEvent event) throws IOException {
         App.setRoot("fxml/Car-view.fxml");
+    }
+
+    /**
+     * Wechselt bei Aufruf die Sprache.
+     * @param event mouse click
+     */
+    @FXML
+    void changeLanguage(MouseEvent event) throws IOException {
+        //Locale locale = isEnglish ? new Locale("de") : new Locale("en");
+
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles/resource_bundle", new Locale("en"));
+
+        System.out.println(bundle.getString("Sprache"));
+
+
+
+        isEnglish = !isEnglish;
     }
 }
