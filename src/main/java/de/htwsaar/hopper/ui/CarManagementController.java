aa24a21db090797bef93fa4c0902f036095d0df3
dgomer <danielgomer25@gmail.com>
2023-03-26 14:57:41 +0200

    Translated: Customer-edit, Customer-creation, Car-edit, Car-creation, ReturnCar
    i18n.properties: updated

--- src/main/java/de/htwsaar/hopper/ui/CarManagementController.java
+++ src/main/java/de/htwsaar/hopper/ui/CarManagementController.java
@@ -56,9 +56,10 @@ public final class CarManagementController implements Initializable {
     @FXML
     void switchToSceneAddCar(ActionEvent event) throws IOException {
         Stage stage = new Stage();
-
+        URL url = getClass().getResource("fxml/Car-creation-view.fxml");
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.i18n");
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/Car-creation-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(url, bundle);
             Parent root1 = (Parent) fxmlLoader.load();
             stage = new Stage();
             stage.setScene(new Scene(root1));
@@ -118,11 +119,12 @@ public final class CarManagementController implements Initializable {
     @FXML
     void switchToSceneUpdateCar(ActionEvent event) throws IOException {
         Stage stage = new Stage();
-
+        URL url = getClass().getResource("fxml/Car-edit-view.fxml");
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.i18n");
         try {
             selectedCar = tableView.getSelectionModel().getSelectedItem();
             setSelectedCar(selectedCar);
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/Car-edit-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(url, bundle);
             Parent root1 = fxmlLoader.load();
             stage = new Stage();
             stage.setScene(new Scene(root1));
@@ -185,20 +187,21 @@ public final class CarManagementController implements Initializable {
 
     @FXML
     public void deleteCar() {
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.i18n");
 
         setSelectedCar(tableView.getSelectionModel().getSelectedItem());
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Wollen Sie den Wagen wirklich löschen?");
-        alert.setHeaderText("Wagen wirklich löschen?");
-        alert.setContentText("Wagen: "+selectedCar.getCarId()+" "+selectedCar.getBrand()+" "+selectedCar.getType());
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, bundle.getString("CAR_CONFIRM_DELETE"));
+        alert.setHeaderText(bundle.getString("CAR_HEADER_CONFIRM_DELETE"));
+        alert.setContentText(bundle.getString("CAR_CONTENT_TEXT")+ " " +selectedCar.getCarId()+" "+selectedCar.getBrand()+" "+selectedCar.getType());
         alert.showAndWait();
         if(alert.getResult().getText().equals("OK"))
         {
         CarRepository.delete(selectedCar);
         reloadTable();
-        Alert alert2 = new Alert(Alert.AlertType.INFORMATION, "Der Wagen wurde gelöscht.");
+        Alert alert2 = new Alert(Alert.AlertType.INFORMATION, bundle.getString("CAR_DELETED"));
         alert2.show();
         } else {
-        Alert alert2 = new Alert(Alert.AlertType.INFORMATION, "Der Wagen wurde nicht gelöscht.");
+        Alert alert2 = new Alert(Alert.AlertType.INFORMATION, bundle.getString("CAR_NOT_DELETED"));
         alert2.show();
         alert.close();
     }
