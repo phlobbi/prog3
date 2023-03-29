4a41220e2911784fca3689b45b361471822f5b4a
Bennet <friendlydiamondlp@gmail.com>
2023-03-29 16:42:31 +0200

    changed car view to fit to booking management

--- src/main/java/de/htwsaar/hopper/ui/CarManagementController.java
+++ src/main/java/de/htwsaar/hopper/ui/CarManagementController.java
@@ -2,7 +2,6 @@ package de.htwsaar.hopper.ui;
 
 import de.htwsaar.hopper.logic.implementations.Car;
 import de.htwsaar.hopper.repositories.CarRepository;
-import de.htwsaar.hopper.repositories.CustomerRepository;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,11 +10,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -27,31 +25,94 @@ public final class CarManagementController implements Initializable {
     private static Car selectedCar;
 
     @FXML
-    private Button btnRead;
-
-    @FXML
     private Button btnCreate;
 
     @FXML
     private Button btnGoBack;
 
     @FXML
+    private Button btnRead;
+
+    @FXML
     private Button btnRemove;
+
+    @FXML
+    private Button btnResetSearch;
+
+    @FXML
+    private Button btnSearch;
 
     @FXML
     private Button btnUpdate;
 
     @FXML
-    private TableView<Car> tableView;
+    private TableColumn<Car, String> carBasePriceColumn;
 
     @FXML
     private TableColumn<Car, String> carBrandColumn;
 
     @FXML
-    private TableColumn<Car, String> carIdColumn;
+    private TableColumn<Car, String> carCurrentPriceColumn;
+
+    @FXML
+    private TableColumn<Car, String> carFuelTypeColumn;
+
+    @FXML
+    private TableColumn<Car, String> carHorsepowerColumn;
+
+    @FXML
+    private TableColumn<Car, String> carIDColumn;
+
+    @FXML
+    private TableColumn<Car, String> carLicensePlateColumn;
+
+    @FXML
+    private TableColumn<Car, String> carMileageColumn;
+
+    @FXML
+    private TableColumn<Car, String> carModelColumn;
+
+    @FXML
+    private TableColumn<Car, String> carSatNavColumn;
+
+    @FXML
+    private TableColumn<Car, String> carTransmissionColumn;
 
     @FXML
     private TableColumn<Car, String> carTypeColumn;
+
+    @FXML
+    private CheckMenuItem filterBrand;
+
+    @FXML
+    private CheckMenuItem filterFuelType;
+
+    @FXML
+    private CheckMenuItem filterModel;
+
+    @FXML
+    private CheckMenuItem filterLicensePlate;
+
+    @FXML
+    private CheckMenuItem filterTransmission;
+
+    @FXML
+    private CheckMenuItem filterType;
+
+    @FXML
+    private MenuButton menuButtonFilter;
+
+    @FXML
+    private MenuItem menuItemUncheck;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private TableView<Car> tableView;
+
+    @FXML
+    private TextField textFieldSearch;
 
     @FXML
     void switchToSceneAddCar(ActionEvent event) throws IOException {
@@ -112,11 +173,6 @@ public final class CarManagementController implements Initializable {
     }
 
     @FXML
-    void switchToSceneRemoveCar(ActionEvent event) {
-
-    }
-
-    @FXML
     void switchToSceneUpdateCar(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         URL url = getClass().getResource("fxml/Car-edit-view.fxml");
@@ -170,9 +226,19 @@ public final class CarManagementController implements Initializable {
     public void reloadTable() {
         tableView.getItems().clear();
 
-        carBrandColumn.setCellValueFactory(new PropertyValueFactory<>("brand"));
+        carIDColumn.setCellValueFactory(new PropertyValueFactory<>("carId"));
         carTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
-        carIdColumn.setCellValueFactory(new PropertyValueFactory<>("carId"));
+        carBrandColumn.setCellValueFactory(new PropertyValueFactory<>("brand"));
+        carModelColumn.setCellValueFactory(new PropertyValueFactory<>("model"));
+        carTransmissionColumn.setCellValueFactory(new PropertyValueFactory<>("transmissionType"));
+        carFuelTypeColumn.setCellValueFactory(new PropertyValueFactory<>("fuelType"));
+        carHorsepowerColumn.setCellValueFactory(new PropertyValueFactory<>("horsepower"));
+        carSatNavColumn.setCellValueFactory(new PropertyValueFactory<>("satNavShowField"));
+        carMileageColumn.setCellValueFactory(new PropertyValueFactory<>("mileage"));
+        carLicensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("licensePlate"));
+        carBasePriceColumn.setCellValueFactory(new PropertyValueFactory<>("basePrice"));
+        carCurrentPriceColumn.setCellValueFactory(new PropertyValueFactory<>("currentPrice"));
+
         ObservableList<Car> observableList = FXCollections.observableArrayList();
         observableList.addAll(CarRepository.findAll());
         tableView.getItems().addAll(observableList);
@@ -196,17 +262,127 @@ public final class CarManagementController implements Initializable {
         alert.showAndWait();
         if(alert.getResult().getText().equals("OK"))
         {
-        CarRepository.delete(selectedCar);
-        reloadTable();
-        Alert alert2 = new Alert(Alert.AlertType.INFORMATION, bundle.getString("CAR_DELETED"));
-        alert2.show();
+            CarRepository.delete(selectedCar);
+            reloadTable();
+            Alert alert2 = new Alert(Alert.AlertType.INFORMATION, bundle.getString("CAR_DELETED"));
+            alert2.show();
         } else {
-        Alert alert2 = new Alert(Alert.AlertType.INFORMATION, bundle.getString("CAR_NOT_DELETED"));
-        alert2.show();
-        alert.close();
+            Alert alert2 = new Alert(Alert.AlertType.INFORMATION, bundle.getString("CAR_NOT_DELETED"));
+            alert2.show();
+            alert.close();
+        }
+
     }
 
-}
+    @FXML
+    void resetSearch(ActionEvent event) {
+        uncheckFilters(new ActionEvent());
+        textFieldSearch.clear();
+        reloadTable();
+    }
+
+    @FXML
+    void searchCars(ActionEvent event) {
+        try{
+            String searchCriteria = textFieldSearch.getText();
+
+            if(searchCriteria.trim().isEmpty()){
+                throw new IllegalArgumentException("Kein Suchkriterium eingegeben");
+            }
+
+            ObservableList<CheckMenuItem> checkMenuItems = FXCollections.observableArrayList();
+            checkMenuItems = getAllSelectedCriteria();
+
+            if (checkMenuItems.isEmpty())
+                throw new IllegalArgumentException("Kein Kriterium ausgewählt");
+
+
+            tableView.getItems().clear();
+            for (Car car : CarRepository.findAvailable()) {
+                for (CheckMenuItem item : checkMenuItems) {
+                    boolean allowedToInsert = false;
+                    if (item.equals(filterBrand)) {
+                        if (car.getBrand().toLowerCase().contains(searchCriteria.toLowerCase()))
+                            allowedToInsert = true;
+                    } else if (item.equals(filterModel)) {
+                        if (car.getModel().toLowerCase().contains(searchCriteria.toLowerCase()))
+                            allowedToInsert = true;
+                    } else if (item.equals(filterType)) {
+                        if (car.getType().getLabel().toLowerCase().contains(searchCriteria.toLowerCase()))
+                            allowedToInsert = true;
+                    } else if (item.equals(filterFuelType)) {
+                        if (car.getFuelType().getLabel().toLowerCase().contains(searchCriteria.toLowerCase()))
+                            allowedToInsert = true;
+                    } else if (item.equals(filterTransmission)) {
+                        if (car.getTransmissionType().getLabel().toLowerCase().contains(searchCriteria.toLowerCase()))
+                            allowedToInsert = true;
+                    } else if (item.equals(filterLicensePlate)) {
+                        if (car.getLicensePlate().toLowerCase().contains(searchCriteria.toLowerCase()))
+                            allowedToInsert = true;
+                    }
+                    if (!IsCarAlreadyInTable(car)){
+                        if (allowedToInsert)
+                            tableView.getItems().add(car);
+                    }
+
+                }
+            }
+        } catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Fehler");
+            alert.setHeaderText("Fehler bei der Suche");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    void searchCarsViaEnter(KeyEvent event) {
+
+    }
+
+
+    /**
+     * Gibt eine Liste aus mit allen Suchkriterien, die ausgewählt sind
+     * @return Liste mit allen ausgewählten Suchkriterien
+     */
+    private ObservableList<CheckMenuItem> getAllSelectedCriteria(){
+        ObservableList<CheckMenuItem> checkMenuItems = FXCollections.observableArrayList();
+
+        if (filterBrand.isSelected())
+            checkMenuItems.add(filterBrand);
+        if (filterModel.isSelected())
+            checkMenuItems.add(filterModel);
+        if (filterType.isSelected())
+            checkMenuItems.add(filterType);
+        if (filterTransmission.isSelected())
+            checkMenuItems.add(filterTransmission);
+        if (filterFuelType.isSelected())
+            checkMenuItems.add(filterFuelType);
+        if (filterLicensePlate.isSelected())
+            checkMenuItems.add(filterLicensePlate);
+
+        return checkMenuItems;
+    }
+
+    @FXML
+    void uncheckFilters(ActionEvent event) {
+        filterBrand.setSelected(false);
+        filterModel.setSelected(false);
+        filterType.setSelected(false);
+        filterFuelType.setSelected(false);
+        filterTransmission.setSelected(false);
+        filterLicensePlate.setSelected(false);
+    }
+
+    /**
+     * Prüft ob das Auto bereits in der Liste ist
+     * @param car Auto was zu prüfen ist
+     * @return true wenn Auto bereits in der Liste ist, sonst false
+     */
+    private boolean IsCarAlreadyInTable(Car car){
+        return tableView.getItems().contains(car);
+    }
 
     /**
      * Setzt die Variable selectedCar auf ein angegebenes Car-Objekt.
