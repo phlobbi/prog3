7e4f3aef0f9a15e75972d0fb585cfe5f21edaad4
htwTD <pib.tolga.dagli@htwsaar.de>
2023-03-31 15:21:31 +0200

    klassen importen die fehlen, kompiliert nun

--- src/main/java/de/htwsaar/hopper/repositories/CustomerRepository.java
+++ src/main/java/de/htwsaar/hopper/repositories/CustomerRepository.java
@@ -3,6 +3,10 @@ package de.htwsaar.hopper.repositories;
 import de.htwsaar.hopper.logic.implementations.Booking;
 import de.htwsaar.hopper.logic.implementations.Customer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 /**
